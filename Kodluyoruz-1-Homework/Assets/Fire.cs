4daa9c0b93601a681d927e40be6f5e0aebda45cb
Yusuffkoc <48139038+Yusuffkoc@users.noreply.github.com>
2020-10-08 21:59:01 +0300

    Initial

---
+++ Kodluyoruz-1-Homework/Assets/Fire.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    
+    public void gun()
+    {
+        Debug.Log("Gun ile Olduruldun.");
+    }
+
+    public void fireGun() 
+    {
+         Debug.Log("FireGun ile Olduruldun.");
+    }
+
+    public void laserFire()
+    {
+        Debug.Log("Laser ile Olduruldun.");
+    }
+}
